9985e6a4c79df97a8c195803f4efbb2deb0f9e5a
Michael Walker <mwalk762@mtroyal.ca>
2020-06-14 00:49:09 -0600

    update to scan range of ports

--- src/main.c
+++ src/main.c
@@ -1,23 +1,45 @@
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <ctype.h>
 #include "tlpi_hdr.h"
-#include "inet_sockets.h"
 
-#define MAX_PORT 65535
+#define BUF_SIZE 10 // Maximum size of messages exchanged  between client and server
 
 int main(int argc, char **argv)
 {
-        int sock_fd;
+        struct sockaddr_in addr;
+        int socket_fd;
+        short port;
 
-        if (argc < 3 || strcmp(argv[1], "--help") == 0)
-                usageErr("Usage: %s host port\n", argv[0]);
+        if (argc < 3 || strcmp(argv[1], "--help") == 0 || strchr(argv[2], ':') == NULL)
+                usageErr("Usage: %s host start_port:end_port\n", argv[0]);
 
-        for (int i = 0; i < 100; i++)
+        char *start_port = strtok(argv[2], ":");
+        char *end_port = strtok(NULL, ":");
+
+        if (end_port == NULL)
+                usageErr("Usage: %s host port_start:port_end\n", argv[0]);
+
+        int n_end_port = getInt(end_port, GN_ANY_BASE, argv[0]);
+
+        for (int current_port = getInt(start_port, GN_ANY_BASE, argv[0]); current_port <= n_end_port; current_port++)
         {
-                sock_fd = inetConnect(argv[1], argv[2], SOCK_STREAM); // tcp
-                if (sock_fd == -1)
-                        continue;
+                socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+                if (socket_fd == -1)
+                        errExit("socket");
 
-                printf("port %d open\n", i);
+                memset(&addr, 0, sizeof(struct sockaddr_in));
+                addr.sin_family = AF_INET;
+                addr.sin_port = htons(current_port);
+                if (inet_pton(AF_INET, argv[1], &addr.sin_addr) <= 0)
+                        fatal("inet_pton failed for address '%s'", argv[1]);
+
+                if (connect(socket_fd, (struct sockaddr *)&addr, sizeof addr) != -1)
+                {
+                        printf("port %d open\n", current_port);
+                        close(socket_fd);
+                }
         }
-
         return 0;
 }
--- tests/main.c
+++ tests/main.c
@@ -1,33 +1,23 @@
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <sys/socket.h>
-#include <ctype.h>
 #include "tlpi_hdr.h"
+#include "inet_sockets.h"
 
-#define BUF_SIZE 10 // Maximum size of messages exchanged  between client and server
+#define MAX_PORT 65535
 
 int main(int argc, char **argv)
 {
-        struct sockaddr_in addr;
-        int socket_fd, j;
-        short port;
-        size_t msg_len;
-        ssize_t num_bytes;
-        char resp[BUF_SIZE];
+        int sock_fd;
 
         if (argc < 3 || strcmp(argv[1], "--help") == 0)
                 usageErr("Usage: %s host port\n", argv[0]);
 
-        port = getInt(argv[2], GN_ANY_BASE, argv[0]);
-        socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-        if (socket_fd == -1)
-                errExit("socket");
+        for (int i = 0; i < 100; i++)
+        {
+                sock_fd = inetConnect(argv[1], argv[2], SOCK_STREAM); // tcp
+                if (sock_fd == -1)
+                        continue;
 
-        memset(&addr, 0, sizeof(struct sockaddr_in));
-        addr.sin_family = AF_INET;
-        addr.sin_port = htons(port);
-        if (inet_pton(AF_INET, argv[1], &addr.sin_addr) <= 0)
-                fatal("inet_pton failed for address '%s'", argv[1]);
+                printf("port %d open\n", i);
+        }
 
         return 0;
 }
