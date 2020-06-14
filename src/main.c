52d07b07ab3c7d8f7ee42412100e1ef85605ab7a
Michael Walker <mwalk762@mtroyal.ca>
2020-06-14 02:12:43 -0600

    Clean up code

--- src/main.c
+++ src/main.c
@@ -11,37 +11,40 @@
 
 int main(int argc, char **argv)
 {
-        struct sockaddr_in addr;
-        int socket_fd;
-
         if (argc < 3 || strcmp(argv[1], "--help") == 0 || strchr(argv[2], ':') == NULL)
                 usageErr("Usage: %s host start_port:end_port\n", argv[0]);
 
-        char *endptr, *range;
+        long start_port = 0;
+        long end_port = 0;
+        char *endptr = NULL;
+        char *range = argv[2];
 
-        range = argv[2];
-
-        errno = 0;
-        long start_port = strtol(range, &endptr, 10);
-        if ((errno == ERANGE && (start_port == LONG_MAX || start_port == LONG_MIN)) || (errno != 0 && start_port == 0))
-                errExit("strtol");
-
-        if (*endptr == '\0' || *endptr != ':')
+        start_port = strtol(range, &endptr, 10);
+        if (*endptr == ':')
+        {
+                end_port = strtol(endptr + 1, &endptr, 10);
+                if (*endptr != '\0')
+                        usageErr("Usage: %s host start_port:end_port\n", argv[0]);
+        }
+        else if (*endptr == '\0')
+                end_port = start_port;
+        else
                 usageErr("Usage: %s host start_port:end_port\n", argv[0]);
-        endptr++;
-        long end_port = getInt(endptr, GN_ANY_BASE, argv[0]);
+
+        struct sockaddr_in addr;
+        if (inet_pton(AF_INET, argv[1], &addr.sin_addr) <= 0)
+                fatal("inet_pton failed for address '%s'", argv[1]);
 
         for (int port = start_port; port <= end_port; port++)
         {
-                socket_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+                int socket_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
                 if (socket_fd == -1)
                         errExit("socket");
 
                 memset(&addr, 0, sizeof(struct sockaddr_in));
                 addr.sin_family = AF_INET;
                 addr.sin_port = htons(port);
-                if (inet_pton(AF_INET, argv[1], &addr.sin_addr) <= 0)
-                        fatal("inet_pton failed for address '%s'", argv[1]);
 
                 if (connect(socket_fd, (struct sockaddr *)&addr, sizeof addr) != -1)
                 {
